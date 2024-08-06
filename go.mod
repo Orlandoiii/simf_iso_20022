4fc961f10d1e6df8771a874910f7a55f9754b05c
Orlando Torres R <orlandoenriqueiii@gmail.com>
2024-08-06 08:46:27 -0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Orlandoiii/simf_iso_20022
+
+go 1.22.3
---
+++ simf_iso_20022.go
@@ -0,0 +1,7 @@
+package simf_iso_20022
+
+import "fmt"
+
+func TestFrmIso() {
+	fmt.Println("Esto es un mensaje desde el repo nuevo")
+}
